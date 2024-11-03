fd7c6f9cf2129adf18a64fd7efd3ae3727a01c68
TkachukNazar <tkachnazar931@gmail.com>
2024-11-03 15:29:50 +0200

    Added files

---
+++ script.js
@@ -0,0 +1,18 @@
+const phrases = [
+  "Наш менеджер передзвонить Вам найближчим часом!",
+  "Уточнити деталі можна за телефоном 123456789",
+  "Залишайтеся на зв'язку!",
+  "З Вами дуже приємно спілкуватися!",
+];
+
+const goodBye = "До побачення, радий був поспілкуватися!";
+let str;
+label: while (true) {
+  str = prompt("Ваше запитання");
+  if (str === "До побачення" || str === "!" || str === "") {
+    alert(goodBye);
+    break;
+  } else {
+    alert(phrases[Math.floor(Math.random() * phrases.length)]);
+  }
+}
